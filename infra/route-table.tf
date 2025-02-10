66c49410b4f88e76101f02faaf67eb06be8356a7
Rauan Santos <rauanmoreira259@gmail.com>
2025-02-09 23:08:07 -0300

    fixes syntax

--- infra/route-table.tf
+++ infra/route-table.tf
@@ -32,7 +32,7 @@ resource "aws_nat_gateway" "example_nat_gw_1" {
 
 resource "aws_nat_gateway" "example_nat_gw_2" {
   allocation_id = aws_eip.nat_2.id
-  subnet_id     = aws_subnet.example_public_subnet_2
+  subnet_id     = aws_subnet.example_public_subnet_2.id
 
   tags = {
     Name = "example-nat-gw-2"
